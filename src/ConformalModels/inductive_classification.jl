326034a8b03478b10f34de93ac9b795bb1f8da29
pat-alt <altmeyerpat@gmail.com>
2022-10-18 14:36:19 +0200

    inductive both adjusted

--- src/ConformalModels/inductive_classification.jl
+++ src/ConformalModels/inductive_classification.jl
@@ -1,34 +1,46 @@
 "A base type for Inductive Conformal Classifiers."
 abstract type InductiveConformalClassifier <: InductiveConformalModel end
 
-"""
-    predict_region(conf_model::InductiveConformalClassifier, Xnew, coverage::AbstractFloat=0.95)
-
-Generic method to compute prediction region for given quantile `q̂` for Inductive Conformal Classifiers. 
-"""
-function predict_region(conf_model::InductiveConformalClassifier, Xnew, coverage::AbstractFloat=0.95)
-    q̂ = empirical_quantile(conf_model, coverage)
-    p̂ = MMI.predict(conf_model.model, conf_model.fitresult, Xnew)
-    L = p̂.decoder.classes
-    ŷnew = pdf(p̂, L)
-    ŷnew = map(x -> collect(key => 1-val <= q̂ ? val : missing for (key,val) in zip(L,x)),eachrow(ŷnew))
-    return ŷnew 
-end
-
 # Simple
 "The `SimpleInductiveClassifier` is the simplest approach to Inductive Conformal Classification. Contrary to the [`NaiveClassifier`](@ref) it computes nonconformity scores using a designated calibration dataset."
 mutable struct SimpleInductiveClassifier{Model <: Supervised} <: InductiveConformalClassifier
     model::Model
     coverage::AbstractFloat
     scores::Union{Nothing,AbstractArray}
+    heuristic::Function
+    train_ratio::AbstractFloat
 end
 
-function SimpleInductiveClassifier(model::Supervised, fitresult=nothing)
-    return SimpleInductiveClassifier(model, fitresult, nothing)
+function SimpleInductiveClassifier(model::Supervised; coverage::AbstractFloat=0.95, heuristic::Function=f(ŷ)=1.0-ŷ, train_ratio::AbstractFloat=0.5)
+    return SimpleInductiveClassifier(model, coverage, nothing, heuristic, train_ratio)
 end
 
 @doc raw"""
-    score(conf_model::SimpleInductiveClassifier, Xtrain, ytrain)
+    MMI.fit(conf_model::SimpleInductiveClassifier, verbosity, X, y)
+
+Wrapper function to fit the underlying MLJ model. For Inductive Conformal Prediction the underlying model is fitted on the *proper training set*. The `fitresult` is assigned to the model instance. Computation of nonconformity scores requires a separate calibration step involving a *calibration data set* (see [`calibrate!`](@ref)). 
+"""
+function MMI.fit(conf_model::SimpleInductiveClassifier, verbosity, X, y)
+    
+    # Data Splitting:
+    train, calibration = partition(eachindex(y), conf_model.train_ratio)
+    Xtrain = MLJ.matrix(X)[train,:]
+    ytrain = y[train]
+    Xcal = MLJ.matrix(X)[calibration,:]
+    ycal = y[calibration]
+
+    # Training: 
+    fitresult, cache, report = MMI.fit(conf_model.model, verbosity, MMI.reformat(conf_model.model, Xtrain, ytrain)...)
+
+    # Nonconformity Scores:
+    ŷ = MMI.predict(conf_model.model, fitresult, Xcal)
+    conf_model.scores = @.(conf_model.heuristic(ycal, ŷ))
+
+    return (fitresult, cache, report)
+end
+
+@doc raw"""
+    MMI.predict(conf_model::SimpleInductiveClassifier, fitresult, Xnew)
 
 For the [`SimpleInductiveClassifier`](@ref) prediction sets are computed as follows,
 
@@ -39,16 +51,14 @@ For the [`SimpleInductiveClassifier`](@ref) prediction sets are computed as foll
 ``
 
 where ``\mathcal{D}_{\text{calibration}}`` denotes the designated calibration data and ``\hat\mu`` denotes the model fitted on training data ``\mathcal{D}_{\text{train}}``.
-
-# Examples
-
-```julia
-conf_model = conformal_model(model; method=:simple)
-score(conf_model, X, y)
-```
 """
-function score(conf_model::SimpleInductiveClassifier, Xcal, ycal)
-    ŷ = pdf.(MMI.predict(conf_model.model, conf_model.fitresult, Xcal),ycal)
-    return @.(1.0 - ŷ)
+function MMI.predict(conf_model::SimpleInductiveClassifier, fitresult, Xnew)
+    p̂ = MMI.predict(conf_model.model, fitresult, MMI.reformat(conf_model.model, Xnew)...)
+    L = p̂.decoder.classes
+    ŷ = pdf(p̂, L)
+    v = conf_model.scores
+    q̂ = qplus(v, conf_model)
+    ŷ = map(x -> collect(key => 1.0-val <= q̂ ? val : missing for (key,val) in zip(L,x)),eachrow(ŷ))
+    return 
 end
 
