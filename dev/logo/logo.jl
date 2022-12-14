1d02211220ed0e90c898fa206c5062887b118bcd
pat-alt <altmeyerpat@gmail.com>
2022-12-14 16:03:17 +0100

    just some formatting

--- dev/logo/logo.jl
+++ dev/logo/logo.jl
@@ -1,5 +1,6 @@
 
-using Pkg; Pkg.activate("dev")
+using Pkg;
+Pkg.activate("dev");
 
 using Colors
 using ConformalPrediction
@@ -15,10 +16,10 @@ const julia_colors = Dict(
     :blue => Luxor.julia_blue,
     :red => Luxor.julia_red,
     :green => Luxor.julia_green,
-    :purple => Luxor.julia_purple
+    :purple => Luxor.julia_purple,
 )
 
-function get_data(N=500; xmax=2.0, noise=0.5, fun::Function=f)
+function get_data(N = 500; xmax = 2.0, noise = 0.5, fun::Function = f)
     # Inputs:
     d = Distributions.Uniform(-xmax, xmax)
     x = rand(d, N)
@@ -30,14 +31,13 @@ function get_data(N=500; xmax=2.0, noise=0.5, fun::Function=f)
     return x, y
 end
 
-function logo_picture(
-    ;
+function logo_picture(;
     ndots = 3,
     frame_size = 500,
-    ms = frame_size//10,
+    ms = frame_size // 10,
     mcolor = (:red, :green, :purple),
     margin = 0.1,
-    fun=f(x) = x * cos(x),
+    fun = f(x) = x * cos(x),
     xmax = 2.5,
     noise = 0.5,
     ged_data = get_data,
@@ -46,7 +46,7 @@ function logo_picture(
     gt_stroke_size = 5,
     interval_color = julia_colors[:blue],
     interval_alpha = 0.2,
-    seed = 2022
+    seed = 2022,
 )
 
     # Setup
@@ -55,37 +55,37 @@ function logo_picture(
     Random.seed!(seed)
 
     # Data
-    x, y = get_data(xmax=xmax, noise=noise, fun=fun)
-    train, test = partition(eachindex(y), 0.4, 0.4, shuffle=true)
-    xtrue = range(-xmax,xmax,ntrue)
+    x, y = get_data(xmax = xmax, noise = noise, fun = fun)
+    train, test = partition(eachindex(y), 0.4, 0.4, shuffle = true)
+    xtrue = range(-xmax, xmax, ntrue)
     ytrue = fun.(xtrue)
 
     # Conformal Prediction
-    Model = @load LinearRegressor pkg=MLJLinearModels
+    Model = @load LinearRegressor pkg = MLJLinearModels
     degree_polynomial = 5
-    polynomial_features(x, degree::Int) = reduce(hcat, map(i -> x.^i, 1:degree))
+    polynomial_features(x, degree::Int) = reduce(hcat, map(i -> x .^ i, 1:degree))
     pipe = (x -> MLJBase.table(polynomial_features(x, degree_polynomial))) |> Model()
-    conf_model = conformal_model(pipe; coverage=0.95)
+    conf_model = conformal_model(pipe; coverage = 0.95)
     mach = machine(conf_model, x, y)
-    fit!(mach, rows=train)
+    fit!(mach, rows = train)
     yhat = predict(mach, x[test])
     y_lb = [y[1] for y in yhat]
     y_ub = [y[2] for y in yhat]
 
     # Logo
-    idx = sample(test, ndots, replace=false)
+    idx = sample(test, ndots, replace = false)
     xplot, yplot = (x[idx], y[idx])
-    _scale = (frame_size/(2*maximum(x))) * (1 - margin)
+    _scale = (frame_size / (2 * maximum(x))) * (1 - margin)
 
     # Ground truth:
     setline(gt_stroke_size)
     sethue(gt_color)
-    true_points = [Point((_scale .* (x,y))...) for (x,y) in zip(xtrue,ytrue)]
+    true_points = [Point((_scale .* (x, y))...) for (x, y) in zip(xtrue, ytrue)]
     poly(true_points[1:(end-1)], action = :stroke)
 
     # Data
-    data_plot = zip(xplot,yplot)
-    for i in 1:length(data_plot)
+    data_plot = zip(xplot, yplot)
+    for i = 1:length(data_plot)
         _x, _y = _scale .* collect(data_plot)[i]
         color_idx = i % n_mcolor == 0 ? n_mcolor : i % n_mcolor
         sethue(mcolor[color_idx]...)
@@ -95,29 +95,39 @@ function logo_picture(
     # Prediction interval:
     _order_lb = sortperm(x[test])
     _order_ub = reverse(_order_lb)
-    lb = [Point((_scale .* (x,y))...) for (x,y) in zip(x[test][_order_lb],y_lb[_order_lb])]
-    ub = [Point((_scale .* (x,y))...) for (x,y) in zip(x[test][_order_ub],y_ub[_order_ub])]
+    lb = [
+        Point((_scale .* (x, y))...) for (x, y) in zip(x[test][_order_lb], y_lb[_order_lb])
+    ]
+    ub = [
+        Point((_scale .* (x, y))...) for (x, y) in zip(x[test][_order_ub], y_ub[_order_ub])
+    ]
     setcolor(sethue(interval_color)..., interval_alpha)
-    poly(vcat(lb, ub), action=:fill)
+    poly(vcat(lb, ub), action = :fill)
 
 end
 
-function draw_small_logo(filename="docs/src/assets/logo.svg";width=500)
+function draw_small_logo(filename = "docs/src/assets/logo.svg"; width = 500)
     frame_size = width
     Drawing(frame_size, frame_size, filename)
     origin()
-    logo_picture(frame_size=frame_size)
+    logo_picture(frame_size = frame_size)
     finish()
     preview()
 end
 
 function draw_wide_logo_new(
-    filename = "docs/src/assets/wide_logo.png"; _pkg_name="Conformal Prediction", font_size=150, font_family="Tamil MN", font_fill="transparent", font_color=Luxor.julia_blue, bg_color="transparent",
-    picture_kwargs...
+    filename = "docs/src/assets/wide_logo.png";
+    _pkg_name = "Conformal Prediction",
+    font_size = 150,
+    font_family = "Tamil MN",
+    font_fill = "transparent",
+    font_color = Luxor.julia_blue,
+    bg_color = "transparent",
+    picture_kwargs...,
 )
 
     # Setup:
-    height = Int(round(font_size*2.4))
+    height = Int(round(font_size * 2.4))
     fontsize(font_size)
     fontface(font_family)
     strs = split(_pkg_name)
@@ -125,8 +135,8 @@ function draw_wide_logo_new(
     width = Int(round(height + text_col_width))
     cw = [height, text_col_width]
     cells = Luxor.Table(height, cw)
-    ms = Int(round(height/10))
-    gt_stroke_size = Int(round(height/50))
+    ms = Int(round(height / 10))
+    gt_stroke_size = Int(round(height / 50))
 
     Drawing(width, height, filename)
     origin()
@@ -136,8 +146,11 @@ function draw_wide_logo_new(
     @layer begin
         translate(cells[1])
         logo_picture(
-            frame_size = height, margin=0.1, ms=ms, gt_stroke_size=gt_stroke_size,
-            picture_kwargs...
+            frame_size = height,
+            margin = 0.1,
+            ms = ms,
+            gt_stroke_size = gt_stroke_size,
+            picture_kwargs...,
         )
     end
 
@@ -150,9 +163,9 @@ function draw_wide_logo_new(
         for (pos, n) in tiles
             @layer begin
                 translate(pos)
-                setline(Int(round(gt_stroke_size/5)))
+                setline(Int(round(gt_stroke_size / 5)))
                 sethue(font_fill)
-                textoutlines(strs[n], O, :path, valign=:middle, halign=:center)
+                textoutlines(strs[n], O, :path, valign = :middle, halign = :center)
                 sethue(font_color)
                 strokepath()
             end
